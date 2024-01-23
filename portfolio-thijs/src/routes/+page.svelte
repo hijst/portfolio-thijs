cf1241047b48459ee977d6be948fb1acb2dd0bde
Thijs Visser <thijsvisser@bol.com>
2024-01-23 03:05:41 +0100

    try new layout

--- portfolio-thijs/src/routes/+page.svelte
+++ portfolio-thijs/src/routes/+page.svelte
@@ -1,37 +1,47 @@
-<script lang="ts">
-	import { posts } from './blog/stores/posts.store';
+<script lang='ts'>
 	import type { Post } from './blog/stores/posts.store';
+	import { posts } from './blog/stores/posts.store';
 	import { DateTime } from 'luxon';
 
 	let latestPost: Post;
 	posts.subscribe(post => {
-		  latestPost = post[0]
-	})
+		latestPost = post[0];
+	});
 </script>
 
-<div class='pt-10 pb-20 pl-20px pr-20px ml-auto mr-auto max-w-[800px] text-6xl font-bold'>
-	<div>Hi! 👋 </div>
-	<div>I'm Thijs. </div>
+<div
+	class='pt-10 pb-10 pl-5 pr-5 ml-auto mr-auto max-w-[800px] text-6xl font-bold flex flex-col items-center'>
 
-	<p class='mt-5'>I am a software engineer working at bol.com</p>
+	<div class='flex mt-10'>
+		<div class='relative text-center'>
+			<div>Hi! 👋</div>
+			<div>I'm Thijs.</div>
+			<div>
+				<img
+					src='https://media.licdn.com/dms/image/D4E03AQEw3P7Apsdx7Q/profile-displayphoto-shrink_800_800/0/1700569439390?e=1711584000&v=beta&t=uQWezz6M_c0MEM7_rnqF8pUzgP9HiKLcEyuZeODmCSo'
+					alt='Avatar' class='rounded-full w-1/6 h-1/6 border-2 border-black dark:border-white mt-10 ml-auto mr-auto'>
+			</div>
+
+			<p class='mt-10'>I am a software engineer working at bol.</p>
+		</div>
+	</div>
 </div>
-
 
 
 <div class='ml-auto mr-auto pt-10 pb-20 max-w-[800px]'>
 
 	{#if latestPost}
-		<h1>Latest blog posts</h1>
+		<h1>Latest blog post</h1>
 		<br />
-		<hr/>
-		<br/>
+		<hr />
+		<br />
 		<div class='pb-5'>
-			<h2><a href="/blog/{latestPost.slug}" style="margin-top: 4px;">{latestPost.title}</a></h2>
-			<br/>
+			<h2><a href='/blog/{latestPost.slug}' style='margin-top: 4px;'>{latestPost.title}</a></h2>
+			<br />
 			<p>Posted on { DateTime.fromJSDate(latestPost.datePublished).toLocaleString(DateTime.DATE_MED) }</p>
 		</div>
 	{:else}
 		<p>Loading...</p>
 	{/if}
-		<hr />
+	<hr />
 </div>
